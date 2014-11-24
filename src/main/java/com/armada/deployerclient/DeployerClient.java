1712140e85f645844ac3b713f6dcf5fa01231ab6
jodavidson <jodavidson@conversantmedia.com>
2014-11-24 00:05:30 -0600

    including jar for demonstration

--- src/main/java/com/armada/deployerclient/DeployerClient.java
+++ src/main/java/com/armada/deployerclient/DeployerClient.java
@@ -20,19 +20,19 @@ public class DeployerClient {
 	    System.out.println("Pull down image from remote registry:");
 	    dockerService.pullImage("armadaproject/agent-prod");
 
-	    String[] agentOneCommand = { "java","-jar", "/opt/lib/agent/agent.jar", "deployer-johntestagent"};
-	    String[] agentTwoCommand = { "java","-jar", "/opt/lib/agent/agent.jar", "deployer-classtest-2"};
-	    String[] agentThreeCommand = { "java","-jar", "/opt/lib/agent/agent.jar", "deployer-classtest-3"};
+	    String[] agentOneCommand = { "java","-jar", "/opt/lib/agent/agent.jar", "TESTdeployer-johntestagent"};
+	    String[] agentTwoCommand = { "java","-jar", "/opt/lib/agent/agent.jar", "TESTdeployer-classtest-2"};
+	    String[] agentThreeCommand = { "java","-jar", "/opt/lib/agent/agent.jar", "TESTdeployer-classtest-3"};
 
 	    String containerOneId = dockerService.createContainer(
 				"armadaproject/agent-prod",
-				"deployer-classtest-1", agentOneCommand);
+				"TESTdeployer-classtest-1", agentOneCommand);
 	    String containerTwoId = dockerService.createContainer(
 			    "armadaproject/agent-prod",
-			    "deployer-classtest-2", agentTwoCommand);
+			    "TESTdeployer-classtest-2", agentTwoCommand);
 	    String containerThreeId = dockerService.createContainer(
 			    "armadaproject/agent-prod",
-			    "deployer-classtest-3", agentThreeCommand);
+			    "TESTdeployer-classtest-3", agentThreeCommand);
 
 	    System.out.println("about to start agents in containers...");
 	    dockerService.startContainer(containerOneId);
